694c5d4ade6a20fe2bab8c0cb9e5b4db040dd14c
Cassandra Gomez <cassandrakgomez@gmail.com>
2024-12-01 01:10:08 -0600

    Gameplay Progress 02 + Instructions
    - Created the bounds for the ship
    - made a pause menu
    - Changed settings to instructions

--- Ocean Game Jam/Assets/Scripts/Ship.cs
+++ Ocean Game Jam/Assets/Scripts/Ship.cs
@@ -17,10 +17,34 @@ public class Ship : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    public void Move(Vector3 movement)
+    //// Update is called once per frame
+   // public void Move(Vector3 movement)
+    //{
+   //     transform.localPosition += movement * 12 * Time.deltaTime;
+//
+   // }
+
+   public void Move(Vector3 movement)
     {
+        // Apply movement
         transform.localPosition += movement * 12 * Time.deltaTime;
+
+        // Get the camera's vertical bounds
+        Camera mainCamera = Camera.main;
+        float screenTop = mainCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y; // Top of the screen
+        float screenBottom = mainCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y; // Bottom of the screen
+
+        //padding
+        screenTop -= 0.6f;
+        screenBottom += 0.8f;
+
+
+        // Clamp the ship's position within the screen's bounds
+        Vector3 clampedPosition = transform.localPosition;
+        clampedPosition.y = Mathf.Clamp(clampedPosition.y, screenBottom, screenTop);
+
+        // Apply the clamped position
+        transform.localPosition = clampedPosition;
     }
 
     
