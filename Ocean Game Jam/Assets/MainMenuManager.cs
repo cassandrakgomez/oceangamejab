890c62e67a2f2733cacb64202a729cc7f4dc04f3
Cassandra Gomez <cassandrakgomez@gmail.com>
2024-11-30 20:32:36 -0600

    Main Menu Progress
    Play button, settings, and quit button working. Title screen has music and the buttons make a noise when hovered over.

---
+++ Ocean Game Jam/Assets/MainMenuManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+        public void StartGame()
+        {
+            Debug.Log("Play Button Working!");
+            SceneManager.LoadScene("Game");
+        }
+        public void QuitGame()
+        {
+            Debug.Log("Quit Button Working!");
+            Application.Quit();
+        }
+        public void Settings()
+        {
+            Debug.Log("Settings Button Working!");
+            SceneManager.LoadScene("Settings");
+        }
+    }
